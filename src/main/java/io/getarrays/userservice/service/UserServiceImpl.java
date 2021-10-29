d66991273e46b197d8942000930aacb030c49916
Maxwel <maxwelsant96@icloud.com>
2021-10-29 11:14:09 -0300

    Add repositories and service

---
+++ src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package io.getarrays.userservice.service;
+
+import io.getarrays.userservice.domain.Role;
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.repo.RoleRepo;
+import io.getarrays.userservice.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService {
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepo.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        User user = userRepo.findByUserName(username);
+        Role role = roleRepo.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        return userRepo.findByUserName(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+}
